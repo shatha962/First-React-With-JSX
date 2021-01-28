38d39d86975c217b34e15488ab9a0c89f398cbaa
Shatha96 <shathabast62@gmail.com>
2021-01-28 17:47:56 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Component } from "react";
+
+class App extends Component {    
+  render(){        
+      return (            
+          <>              
+              <h1>Hello Dojo!</h1>                
+              <h3>Things i need to do</h3>       
+              <ul>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>
+                </ul>  
+                    
+          </>        
+      );    
+  }
+}
+
+export default App;
